cea4588fa9ea0eeb2711d7877e896e62bf4b695c
Alejandro Ibarra <coodingalex@gmail.com>
2021-02-13 15:14:34 -0700

    ready

--- src/utils/api/upload.js
+++ src/utils/api/upload.js
@@ -4,9 +4,15 @@ const URL = process.env.API_URL
 
 export function uploadImage(data, config, setters) {
   setters.setIsLoading(true)
-  axios.post(URL, data, config).then((response) => {
-    setters.setImageUrl(response.data.url)
-    setters.setIsLoading(false)
-    setters.setIsUploaded(true)
-  })
+  axios
+    .post(
+      'https://image-uploader-dev-challenges-backend.vercel.app/upload',
+      data,
+      config
+    )
+    .then((response) => {
+      setters.setImageUrl(response.data.url)
+      setters.setIsLoading(false)
+      setters.setIsUploaded(true)
+    })
 }
